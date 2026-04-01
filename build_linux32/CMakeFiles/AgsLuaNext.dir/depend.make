eec1f150b48bee741981ba9d8fa9c3a691b34c61
sqpp <sqpp15@gmail.com>
2026-04-01 17:53:53 +0200

    1.2.1

--- build_linux32/CMakeFiles/AgsLuaNext.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for AgsLuaNext.
-# This may be replaced when dependencies are built.
--- build_linux64/CMakeFiles/AgsLuaNext.dir/src/dllmain.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/AgsLuaNext.dir/src/dllmain.cpp.o: \
- /mnt/e/Development/Plugins/AgsLuaNext/src/dllmain.cpp \
- /usr/include/stdc-predef.h
